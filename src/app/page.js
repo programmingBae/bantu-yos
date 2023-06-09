dda1071d11745437854a72ada8de1d5624f6f4c8
programmingBae <1972009@maranatha.ac.id>
2023-06-09 17:09:56 +0700

    add SMAN 2,3,5

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 export default function Home() {
   const [data, setData] = useState([]);
   const [sortedData, setSortedData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [sortDirection, setSortDirection] = useState('asc');
 
   useEffect(() => {
@@ -24,6 +25,49 @@ export default function Home() {
     }
   }
 
+  async function fetchNewData() {
+    try {
+      setIsLoading(true);
+      const response = await fetch('https://api.ppdb.jabarprov.go.id/portal/registrant?page=1&limit=500&pagination=false&columns[0][key]=name&columns[0][searchable]=true&columns[1][key]=registration_number&columns[1][searchable]=true&filters[0][key]=first_school.npsn&filters[0][value]=20224112&filters[1][key]=option_type&filters[1][value]=prestasi-rapor&filters[2][key]=first_option.major_id&filters[2][value]=');
+      const newData = await response.json();
+      setData(newData.result.itemsList);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching new data:', error);
+      setIsLoading(false);
+    }
+  }
+
+  async function fetchNewDataSman3() {
+    try {
+      setIsLoading(true);
+      const response = await fetch('https://api.ppdb.jabarprov.go.id/portal/registrant?page=1&limit=500&pagination=false&columns[0][key]=name&columns[0][searchable]=true&columns[1][key]=registration_number&columns[1][searchable]=true&filters[0][key]=first_school.npsn&filters[0][value]=20224114&filters[1][key]=option_type&filters[1][value]=prestasi-rapor&filters[2][key]=first_option.major_id&filters[2][value]=');
+      const newData = await response.json();
+      setData(newData.result.itemsList);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching new data:', error);
+      setIsLoading(false);
+    }
+  }
+
+  async function fetchNewDataSman2() {
+    try {
+      setIsLoading(true);
+      const response = await fetch('https://api.ppdb.jabarprov.go.id/portal/registrant?page=1&limit=500&pagination=false&columns[0][key]=name&columns[0][searchable]=true&columns[1][key]=registration_number&columns[1][searchable]=true&filters[0][key]=first_school.npsn&filters[0][value]=20224139&filters[1][key]=option_type&filters[1][value]=prestasi-rapor&filters[2][key]=first_option.major_id&filters[2][value]=');
+      const newData = await response.json();
+      setData(newData.result.itemsList);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching new data:', error);
+      setIsLoading(false);
+    }
+  }
+
+
+
+  
+
   function sortData() {
     const sorted = [...data].sort((a, b) => {
       if (sortDirection === 'asc') {
@@ -39,9 +83,34 @@ export default function Home() {
     setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc');
   }
 
+  function MouseOver(event) {
+    event.target.style.background = 'white';
+    event.target.style.color = 'black';
+  }
+
+  function MouseOut(event){
+    event.target.style.background="black";
+    event.target.style.color='white';
+  }
+
   return (
     <div>
-      <h1>SMANSA SCORE</h1>
+      <h1>SMA SCORE</h1>
+      <div className="button-container" style={{justifyContent: 'space-between'}}>
+      <button onMouseOut={MouseOut} onMouseOver={MouseOver} onClick={fetchData} disabled={isLoading} style={{padding: '1rem', backgroundColor: 'black', color: 'white'}}>
+          {isLoading ? 'Loading...' : 'SMAN 1'}
+        </button>
+        <button onMouseOut={MouseOut} onMouseOver={MouseOver} onClick={fetchNewDataSman2} disabled={isLoading} style={{padding: '1rem', backgroundColor: 'black', color: 'white'}}>
+          {isLoading ? 'Loading...' : 'SMAN 2'}
+        </button>
+        <button onMouseOut={MouseOut} onMouseOver={MouseOver} onClick={fetchNewDataSman3} disabled={isLoading} style={{padding: '1rem', backgroundColor: 'black', color: 'white'}}>
+          {isLoading ? 'Loading...' : 'SMAN 3'}
+        </button>
+        <button onMouseOut={MouseOut} onMouseOver={MouseOver} onClick={fetchNewData} disabled={isLoading} style={{padding: '1rem', backgroundColor: 'black', color: 'white'}}>
+          {isLoading ? 'Loading...' : 'SMAN 5'}
+        </button>
+      </div>
+      <div className="table-container">
       <table>
         <thead>
           <tr>
@@ -79,7 +148,9 @@ export default function Home() {
           ))}
         </tbody>
       </table>
-      {data.length === 0 && <p>Loading data...</p>}
+      </div>
+      {data.length === 0 && !isLoading && <p>No data available.</p>}
+      {isLoading && <p>Loading data...</p>}
       <style jsx>{`
         .table-container {
           overflow-x: auto;
